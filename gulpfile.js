17bbd6dd2d0e21b40aa897ec24378e3c1140811b
Unknown <gersoncalienes@gmail.com>
2017-11-26 19:35:28 -0500

    Added assets and updated button and hero image

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,30 @@
 const gulp = require('gulp');
 const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars =require('postcss-simple-vars');
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
 
-gulp.task
+gulp.task('default', function (){
+  console.log("horry - you created a gulp task");
+});
+
+gulp.task('html', function() {
+  console.log("imagine doing something useful with your html");
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles')); //return is used for asyncronous function
+});
+
+gulp.task('watch', function(){
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  })
+});
