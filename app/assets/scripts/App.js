6da08ef0950af4944f77e1278bf241ca90c8cf30
Gerson Calienes <gersoncalienes@gmail.com>
2017-11-29 13:20:01 -0500

    completed reveal animation behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu  from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
+// const revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "65%");
+new RevealOnScroll($(".testimonial"), "60%");
